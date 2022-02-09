99f306b165fe1a6ebdc818a5031323373d36157e
BabbaRegina <irene.nicolini@gmail.com>
2022-02-09 20:54:00 +0100

    fix(landing): get static props from CSM

--- content.ts
+++ content.ts
@@ -2,28 +2,28 @@ export const home = {
   draft: {
     hero: {
       title: 'Hmm, need something clever here',
-      body: 'blah blah blah, our product is the best!',
+      body: 'blah blah blah, our product is the best!'
     },
     features: [
       { title: 'Feature 1', body: 'something dope about feature 1 here soon.' },
-      { title: 'Feature 2', body: 'something dope about feature 2 here soon.' },
-    ],
+      { title: 'Feature 2', body: 'something dope about feature 2 here soon.' }
+    ]
   },
   published: {
     hero: {
       title: 'A beautiful knowledge base for your whole team.',
-      body: 'High performing teams use Known to document and record everything. Some other cool SaaS tag line here.',
+      body: 'High performing teams use Known to document and record everything. Some other cool SaaS tag line here.'
     },
     features: [
       {
         title: 'Next gen editor',
         body:
-          'Forget about markdown and rich text. Our editor is superchared to handle any content you can throug at it.',
+          'Forget about markdown and rich text. Our editor is superchared to handle any content you can throug at it.'
       },
       { title: 'Stay organized', body: 'Use folders to put your docs right where you need them when you need them.' },
-      { title: 'Beautiful design', body: 'Award wining design that you will fall in love with.' },
-    ],
-  },
+      { title: 'Beautiful design', body: 'Award wining design that you will fall in love with.' }
+    ]
+  }
 }
 
 export const posts = {
@@ -43,7 +43,7 @@ summary: Will come up with summary later
 slug: why-you-should-write-down-everything
 publsihedOn: ''
 ---
-## Writing is fun`,
+## Writing is fun`
   ],
   published: [
     `---
@@ -60,6 +60,6 @@ summary: If you write it down, you won't forget.
 slug: why-you-should-write-down-everything
 publsihedOn: '3-20-2020'
 ---
-## Elon musk writes`,
-  ],
+## Elon musk writes`
+  ]
 }

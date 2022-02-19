da179a94eec3fa445626c94bbdfd00d3c17ed97b
BabbaRegina <irene.nicolini@gmail.com>
2022-02-19 16:49:27 +0100

    fix(blog): link data from cms and fs for single blog pages

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,9 +3,14 @@ import hydrate from 'next-mdx-remote/hydrate'
 import { majorScale, Pane, Heading, Spinner } from 'evergreen-ui'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
+import renderToString from 'next-mdx-remote/render-to-string'
 import { Post } from '../../types'
 import Container from '../../components/container'
 import HomeNav from '../../components/homeNav'
+import { posts as postsFromCMS } from '../../content'
 
 const BlogPost: FC<Post> = ({ source, frontMatter }) => {
   const content = hydrate(source)
@@ -49,4 +54,44 @@ BlogPost.defaultProps = {
  * then the the correct post for the matching path
  * Posts can come from the fs or our CMS
  */
+
+export function getStaticPaths () {
+  const postsPath = path.join(process.cwd(), 'posts')
+  const fileNames = fs.readdirSync(postsPath)
+
+  const slugs = fileNames.map(name => {
+    const fullPath = path.join(process.cwd(), 'posts', name)
+    const file = fs.readFileSync(fullPath, 'utf-8')
+    const { data } = matter(file)
+    return data
+  })
+
+  return {
+    paths: slugs.map(s => ({ params: { slug: s.slug } })),
+    fallback: true
+  }
+ }
+
+export async function getStaticProps ({ params = {} }) {
+  let post
+  try {
+    const filePath = path.join(process.cwd(), 'posts', `${params.slug}.mdx`)
+    post = fs.readFileSync(filePath, 'utf-8')
+  } catch (error) {
+    const postsCMS = postsFromCMS.published.map(post => matter(post))
+
+    const match = postsCMS.find(p => p.data.slug === params.slug)
+    post = match?.content || ''
+  }
+
+  const { data } = matter(post)
+  const mdxSounce = await renderToString(post, { scope: data })
+  return {
+    props: {
+      source: mdxSounce,
+      frontMatter: data
+    }
+  }
+}
+
 export default BlogPost
